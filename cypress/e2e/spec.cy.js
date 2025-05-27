bd742e959179ce75ca06cfd8d146a5cb1f43d89f
Jair Cunha <Jair@pbsenac.intra>
2025-05-27 09:45:39 -0300

    feat: Cypress installed in the project

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,6 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://www.state.gov/students/')
+    // cy.get('.is-style-outline--1 > .wp-block-button__link').click()
+  })
+})
