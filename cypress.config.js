5ac4d683b593d17f80f5da3f17b0a3c96fa1accf
Jair Cunha <Jair@pbsenac.intra>
2025-05-28 11:41:05 -0300

    feat: Configurações do Cucumber prontas

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
+// const cucumber = require('cypress-cucumber-preprocessor/steps');
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      // on('file:preprocessor', cucumber())
+
       // implement node event listeners here
     },
+    // specPattern: "cypress/e2e/step_definitions/*.feature"
   },
 });
